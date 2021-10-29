8192c064b4b1e9e64bb2add2224c2877d63c7001
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-28 22:03:46 -0700

    Added AppointmentService and VMs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace AppointmentScheduling.Controllers
                     Name = model.Name
                 };
 
-                var result = await _userManager.CreateAsync(user);
+                var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(user, model.RoleName);
